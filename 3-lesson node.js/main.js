157bf610ba3510e62a792823f14842f1350d7c93
Devolimjon <100147793+Devolimjon@users.noreply.github.com>
2022-03-03 12:30:50 +0500

    Add files via upload

---
+++ 3-lesson node.js/main.js
@@ -0,0 +1,5 @@
+const balance = document.querySelector('.balance');
+const result = document.querySelector('.result');
+const bid = document.querySelector('.bid');
+const number = document.querySelector('.number');
+const button = document.querySelector('.btn.btn-primary');
